77d9175b8976af383212370f8ce0ed26b387351f
anshullaxane <anshullaxane@gmail.com>
2025-12-16 16:48:18 +0530

    Add blast_run.py for running BLAST commands
    This file provides a basic structure to run BLAST using subprocess with CLI arguments for input genome, database path, and output file.

---
+++ src/blast_run.py
@@ -0,0 +1,11 @@
+Purpose:
+Run BLAST.
+Contains:
+Code to call blastn using subprocess
+CLI arguments:
+input genome
+database path
+output file
+No filtering, no logic
+Why separate?
+So BLAST can be replaced later if needed.
