a5fbeefd69c0c01372c4b611ebc3783d4d0bffbd
anshullaxane <anshullaxane@gmail.com>
2025-12-16 17:08:46 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,18 @@
+Purpose:
+Interactive interface (Streamlit).
+
+Contains:
+
+File upload widget
+
+Calls pipeline functions
+
+Displays:
+
+tables
+
+plots
+
+interpretation
+
+No heavy computation here.
